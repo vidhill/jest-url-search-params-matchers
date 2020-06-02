9718103537289280f37ad6ba0fe48ebc230facda
David Hill <dh366k@att.com>
2020-06-03 00:24:12 +0100

    Combine and export rules

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import toIncludeQueryParam from './to-include-query-param';
+import toMatchQueryParam from './to-match-query-param';
+
+const rules = {
+  toIncludeQueryParam,
+  toMatchQueryParam,
+};
+
+export default rules;
